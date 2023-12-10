bceadbbea0a2f42b34c3aa62fc6ec4af72b95c3c
McKenzie Jones <mckenzbeck12@gmail.com>
2023-12-10 12:53:17 -0700

    Create route back to homepage

--- src/components/ShopPage/ShopPage.jsx
+++ src/components/ShopPage/ShopPage.jsx
@@ -1,6 +1,12 @@
+import NavBar from "../Navigation/NavBar";
+import { useState } from "react";
+
 const ShopPage = () => {
+  const [numOfItemsInCart, setNumOfItemsInCart] = useState(0);
+
   return (
     <div>
+      <NavBar numOfItemsInCart={numOfItemsInCart}></NavBar>
       <h1>hello from the shop page!</h1>
       <p>more items will be listed here</p>
     </div>
